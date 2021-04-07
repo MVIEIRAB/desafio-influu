f8f524758653315b5bf8a0fabe411953af42655c
Matheus V. B <57693776+MVIEIRAB@users.noreply.github.com>
2021-04-07 19:46:46 -0300

    alterando porta para deploy do heroku

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ const controller = require('./modules/controller')
 
 const app = restify.createServer()
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
 
 app.get('/', controller.getUsers)
